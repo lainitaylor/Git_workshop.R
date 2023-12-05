3d0ae7fee3a907b167b484ca4e20bbd1ef4cb2f4
Laini Taylor <laini.taylor@mail.utoronto.ca>
2023-12-05 10:46:04 -0500

    checked spelling; adding and removing text for practice committing

--- GitWorkshop.R
+++ GitWorkshop.R
@@ -1,4 +1,9 @@
-# Github and reproducability Workshop Dec 5, 2023
+# Github and reproducibility Workshop Dec 5, 2023
+
+# script to keep track of coding notes from workshop and practice
+
 
 ## gitignore can be used to remove a file from git committing
 # done by typing filename into git ignore terminal
+
+## change test
